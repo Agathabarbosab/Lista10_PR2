09eb1d7c8aef5aa782735edbabc23aba5d84c4e1
agathabarbosab <agatha.beatriz@aluno.ifsp.edu.br>
2023-06-22 16:36:46 -0300

    exercicio 2

--- Lista10_PR2/Program.cs
+++ Lista10_PR2/Program.cs
@@ -8,6 +8,107 @@ namespace Lista10_PR2
 {
     internal class Program
     {
+        public static void Exercicio1()
+        {
+            float main = int.MinValue, menon = int.MaxValue, soma = 0, media = 0;
+            int n;
+
+            Console.WriteLine("Digite o tamanho do vetot:");
+            n = int.Parse(Console.ReadLine());
+            float[] vet = new float[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite um valor:");
+                vet[i] = int.Parse(Console.ReadLine());
+                if (vet[i] > main)
+                {
+                    main = vet[i];
+
+                }
+                if (vet[i] < menon)
+                {
+                    menon = vet[i];
+
+                }
+                Console.WriteLine("menor numero:" + menon);
+                Console.WriteLine("maior numero: " + main);
+                
+            }
+            for (int i = 0; i < n; i++)
+            {
+
+                soma += vet[i];
+
+
+            }
+
+
+
+            media = soma / n;
+            Console.WriteLine("media: " + media);
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("vetor[{0}]={1}", i, vet[i]);
+            }
+
+
+
+
+        }
+
+
+public static void Exercicio2()
+        {
+             
+            float media = 0, soma = 0;
+
+            Console.WriteLine("Digite o tamanho do vetor:");
+            int n = int.Parse(Console.ReadLine());
+
+
+            float[] vet = new float[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite um valor:");
+                vet[i] = int.Parse(Console.ReadLine());
+                soma += vet[i];
+            }
+
+
+            media = soma / n;
+            Console.WriteLine("media: " + media);
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+
+                if (vet[i] >= media)
+                    Console.WriteLine("numero maior ou igual a media vetor [{0}]={1}", i, vet[i]);
+
+
+            }
+
+
+        }
+        public static void Exercicio3()
+        {
+
+        }
+        public static void Exercicio4()
+        {
+
+        }
+        public static void Exercicio5()
+        {
+
+        }
         static void Main(string[] args)
         {
             int exercicio;
@@ -18,6 +119,7 @@ namespace Lista10_PR2
                 Console.WriteLine("exercicio 2 - Digite2");
                 Console.WriteLine("exercicio 3 - Digite3");
                 Console.WriteLine("exercicio 4 - Digite4");
+                Console.WriteLine("exercicio 5 - Digite5");
                 Console.WriteLine("Digite 0 - caso queira sair:");
 
                 exercicio = int.Parse(Console.ReadLine());
@@ -39,8 +141,10 @@ namespace Lista10_PR2
                     case 4:
                         Exercicio4();
                         break;
-
-                        ;
+                    case 4:
+                        Exercicio5();
+                        break;
+                        
 
 
                     default:
